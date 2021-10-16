af28eae2828028ff9031f362a03bc30ac785a55d
Michael Longerich <mlongerich@gmail.com>
2021-10-16 23:04:19 +0700

    Update cancel and success pages with photo

--- src/pages/kidz-club-cancel.js
+++ src/pages/kidz-club-cancel.js
@@ -1,34 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Cancel = () => {
   return (
     <Layout>
-      <div className="container pb-12 mx-auto">
-        <h1 className="py-4 text-2xl font-bold text-red-500 font-display">
-          Your payment did not succeed
-        </h1>
-        <div className="mx-auto">
-          <p className="py-2">
-            Your payment was unsuccessful and you were not charged anything.
-          </p>
-          <p className="pt-2 pb-4">
-            Please email{" "}
-            <a
-              className="text-blue-400 underline"
-              href="mailto:kidzclubhh@gmail.com"
-            >
-              kidzclubhh@gmail.com
-            </a>{" "}
-            if you have any questions.
-          </p>
+      <div className="container relative pb-12 mx-auto text-center">
+        <div className="relative z-20">
+          <h1 className="py-4 pt-40 text-2xl font-bold text-red-500 font-display">
+            Oops!
+          </h1>
+          <div className="mx-auto">
+            <p className="py-2">
+              Your payment was unsuccessful <br />
+              and you were not charged.
+            </p>
+            <p className="pt-2 pb-4">
+              Please email{" "}
+              <a
+                className="text-blue-400 underline"
+                href="mailto:kidzclubhh@gmail.com"
+              >
+                kidzclubhh@gmail.com
+              </a>{" "}
+              <br />
+              if you have any questions.
+            </p>
+          </div>
           <Link
             to="/kidz-club"
-            className="inline-block p-2 px-8 py-4 text-sm tracking-widest text-white uppercase bg-blue-500 font-heading"
+            className="inline-block p-2 px-8 py-4 mb-20 text-sm tracking-widest text-white uppercase bg-blue-500 font-heading"
           >
-            Go back to Kidz Club to try again
+            Go back to Kidz Club
           </Link>
+        </div>
+        <div className="absolute inset-0 z-10 object-cover w-full h-full">
+          <StaticImage src="../images/box.png" />
         </div>
       </div>
     </Layout>
--- src/pages/kidz-club-success.js
+++ src/pages/kidz-club-success.js
@@ -1,38 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Success = () => {
   return (
     <Layout>
-      <div className="container pb-12 mx-auto">
-        <h1 className="py-4 text-2xl font-bold text-red-500 font-display">
-          Thank you
-        </h1>
-        <div className="mx-auto">
-          <p className="py-2">
-            Thank you for signing up for our Kidz Club Program.
-          </p>
-          <p className="py-2">Your payment was successfully recieved.</p>
-          <p className="py-2">You will recieve your first package soon.</p>
-          <p className="pt-2 pb-4">
-            Please email{" "}
-            <a
-              className="text-blue-400 underline"
-              href="mailto:kidzclubhh@gmail.com"
-            >
-              kidzclubhh@gmail.com
-            </a>{" "}
-            if you have any questions.
-          </p>
-        </div>
+      <div className="container relative pb-12 mx-auto text-center">
+        <div className="relative z-20">
+          <h1 className="py-4 mt-2 text-2xl font-bold text-red-500 pt-36 font-display">
+            Welcome to the
+            <br /> Kidz Club!
+          </h1>
+          <div className="mx-auto">
+            <p className="py-2">
+              Thank you for signing up for our <br />
+              Kidz Club Program.
+            </p>
+            <p className="py-2">
+              Look for your first email on the
+              <br />
+              1st or 15th of the month.
+            </p>
+            <p className="pt-2 pb-4">
+              Please email{" "}
+              <a
+                className="text-blue-400 underline"
+                href="mailto:kidzclubhh@gmail.com"
+              >
+                kidzclubhh@gmail.com
+              </a>{" "}
+              <br />
+              if you have any questions.
+            </p>
+          </div>
 
-        <Link
-          to="/kidz-club"
-          className="inline-block p-2 px-8 py-4 text-sm tracking-widest text-white uppercase bg-blue-500 font-heading"
-        >
-          Go back to the Kidz Club
-        </Link>
+          <Link
+            to="/kidz-club"
+            className="inline-block p-2 px-8 py-4 text-sm tracking-widest text-white uppercase bg-blue-500 font-heading"
+          >
+            Go back to the Kidz Club
+          </Link>
+        </div>
+        <div className="absolute inset-0 z-10 object-cover w-full h-full">
+          <StaticImage src="../images/box.png" />
+        </div>
       </div>
     </Layout>
   )
